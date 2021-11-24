587ce64b4fbfc09f1436ed1f9e8bb8dead4972d4
Stephen Sorensen <spudly@users.noreply.github.com>
2021-11-24 16:08:09 -0500

    fix test in github action

--- base/jest.config.cjs
+++ base/jest.config.cjs
@@ -4,4 +4,6 @@ const baseConfig = require('../jest.config.base.cjs');
 module.exports = {
   ...baseConfig,
   displayName: '@shuoink/ui-base',
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
--- website/jest.config.cjs
+++ website/jest.config.cjs
@@ -3,4 +3,5 @@ const baseConfig = require('../jest.config.base.cjs');
 
 module.exports = {
   ...baseConfig,
+  collectCoverageFrom: ['pages/**/*.{js,jsx,ts,tsx}'],
 };
