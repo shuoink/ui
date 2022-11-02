5522e061ff223248605eafe5236cf9265dfe30a1
Stephen Sorensen <spudly@users.noreply.github.com>
2022-11-01 21:21:44 -0400

    chore(deps): upgrade dependencies
    BREAKING CHANGE: upgraded dependencies

--- src/components/layout/Accordion.tsx
+++ src/components/layout/Accordion.tsx
@@ -1,4 +1,4 @@
-import type {FC, ReactNode} from 'react';
+import type {ComponentProps, FC, ReactElement, ReactNode} from 'react';
 import {Children, cloneElement, isValidElement, useState} from 'react';
 import AccordionSection from './AccordionSection.js';
 
@@ -12,14 +12,18 @@ const Accordion: FC<{children?: ReactNode}> = ({children}) => {
         const isLast = index === childArray.length - 1;
         const isOpen = activeIndex === index;
         if (isValidElement(child) && child.type === AccordionSection) {
-          return cloneElement(child, {
-            isOpen,
-            toggle: () => setActiveIndex(activeIndex === index ? null : index),
-            dock: {
-              top: !isFirst,
-              bottom: !isLast || isOpen,
+          return cloneElement(
+            child as ReactElement<ComponentProps<typeof AccordionSection>>,
+            {
+              isOpen,
+              toggle: () =>
+                setActiveIndex(activeIndex === index ? null : index),
+              dock: {
+                top: !isFirst,
+                bottom: !isLast || isOpen,
+              },
             },
-          });
+          );
         }
         return child;
       })}
